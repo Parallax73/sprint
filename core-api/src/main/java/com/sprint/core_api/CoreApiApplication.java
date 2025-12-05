0481a87a873ddf273e82eede97eff2a53df39a10
Parallax73 <narakagaara@gmail.com>
2025-12-05 20:45:50 -0300

    feat(core-api): add initial service structure

---
+++ core-api/src/main/java/com/sprint/core_api/CoreApiApplication.java
@@ -0,0 +1,15 @@
+package com.sprint.core_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApiApplication.class, args);
+	}
+
+}
