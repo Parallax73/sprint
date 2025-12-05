b261303b57d6714559919e9847b6d3383989e8ea
Parallax73 <narakagaara@gmail.com>
2025-12-05 16:04:43 -0300

    Added parent pom for better dependency management

--- discovery/src/main/java/com/sprint/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/sprint/discovery/DiscoveryApplication.java
@@ -3,9 +3,10 @@ package com.sprint.discovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
