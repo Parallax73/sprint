0ce76289c3e5b567e3085494078c1281b8362ab4
Parallax73 <narakagaara@gmail.com>
2025-12-07 13:47:45 -0300

    feat(core-api): created UserRepository

---
+++ core-api/src/main/java/com/sprint/core_api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sprint.core_api.repository;
+
+import com.sprint.core_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username);
+}
