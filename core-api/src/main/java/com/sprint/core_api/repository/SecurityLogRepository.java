220191cab1b629bb93b69199ff3a8fddef799d9a
Parallax73 <narakagaara@gmail.com>
2025-12-09 19:29:19 -0300

    feat(core-api): annotate SecurityLogRepository with @Repository for proper bean registration

--- core-api/src/main/java/com/sprint/core_api/repository/SecurityLogRepository.java
+++ core-api/src/main/java/com/sprint/core_api/repository/SecurityLogRepository.java
@@ -4,10 +4,12 @@ import com.sprint.core_api.entity.SecurityLog;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface SecurityLogRepository extends JpaRepository<SecurityLog, UUID> {
 
     List<SecurityLog> findByUsernameOrderByCreatedAtDesc(String username);
