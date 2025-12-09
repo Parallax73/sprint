4865c2ac4262d0c5ef4fe388f763a7fb8b813e33
Parallax73 <narakagaara@gmail.com>
2025-12-09 16:24:44 -0300

    feat(core-api): add method to fetch all active detection rules in DetectionRuleRepository

--- core-api/src/main/java/com/sprint/core_api/repository/DetectionRuleRepository.java
+++ core-api/src/main/java/com/sprint/core_api/repository/DetectionRuleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -12,4 +13,5 @@ public interface DetectionRuleRepository extends JpaRepository<DetectionRule, UU
 
     Optional<DetectionRule> findByName(String name);
 
+    List<DetectionRule> findAllByIsActiveTrue();
 }
