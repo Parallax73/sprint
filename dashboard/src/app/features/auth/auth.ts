c067b05ed9cbaf28d5ca0a1a8323eedd7fb9e27c
Parallax73 <narakagaara@gmail.com>
2025-12-11 20:16:55 -0300

    feat(auth): enhance form UI/UX with dynamic login/register state and PrimeNG components

--- dashboard/src/app/features/auth/auth.ts
+++ dashboard/src/app/features/auth/auth.ts
@@ -2,6 +2,7 @@ import { Component, signal} from '@angular/core';
 import {ButtonDirective, ButtonModule} from "primeng/button";
 import {Checkbox, CheckboxModule} from "primeng/checkbox";
 import {InputText, InputTextModule} from "primeng/inputtext";
+import {PasswordModule} from "primeng/password";
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 
@@ -9,19 +10,32 @@ import {FormsModule} from '@angular/forms';
   selector: 'app-login',
   standalone: true,
   imports: [
-        ButtonDirective,
-        Checkbox,
-        InputText,
-        CommonModule,
-        FormsModule,
-        ButtonModule,
-        CheckboxModule,
-        InputTextModule
-
-    ],
+    ButtonDirective,
+    Checkbox,
+    InputText,
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    CheckboxModule,
+    InputTextModule,
+    PasswordModule
+  ],
   templateUrl: './auth.html',
   styleUrl: './auth.scss',
 })
 export class Auth {
+
+
+  // Auth state true = login, false = register
+  authState = signal<boolean>(true);
+
+  // Remember me checker
   checked = signal<boolean>(true);
+  password = signal<string>('');
+
+  authStateChange(){
+    this.authState.update(state => !state);
+  }
+
+
 }
