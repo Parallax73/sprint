2b04f243e722064735be54b41a2748e3485a3adf
Parallax73 <narakagaara@gmail.com>
2025-12-11 20:16:34 -0300

    feat(routes): add default route to redirect to login page

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
 
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
     path: 'login',
     loadComponent: () => import('./features/auth/auth').then(m => m.Auth)
   }
-
-
-
-
 ];
