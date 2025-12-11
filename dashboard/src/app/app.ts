bae88e8cb6bdbbb211162e78270ca6ed76546340
Parallax73 <narakagaara@gmail.com>
2025-12-11 18:19:47 -0300

    refactor(app): remove unused imports from app.ts

--- dashboard/src/app/app.ts
+++ dashboard/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {Button} from 'primeng/button';
 import {Auth} from './features/auth/auth';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button, Auth],
+  imports: [Auth],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
