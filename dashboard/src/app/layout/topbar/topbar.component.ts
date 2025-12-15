e9d68a16cb99af7b79ef9ce8cfa9ff27891be36c
Parallax73 <narakagaara@gmail.com>
2025-12-15 16:47:15 -0300

    feat(topbar): add navigation link to logo for easier access to Home page

--- dashboard/src/app/layout/topbar/topbar.component.ts
+++ dashboard/src/app/layout/topbar/topbar.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { LayoutService } from '../../core/services/layout.service';
 import {Dialog} from 'primeng/dialog';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [ButtonModule, Dialog],
+  imports: [ButtonModule, Dialog, RouterLink],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss'
 })
@@ -14,6 +15,7 @@ export class TopbarComponent {
   layoutService = inject(LayoutService);
 
   isHelpVisible = false;
+  protected home = '/home'
 
   showDialog() {
     this.isHelpVisible = true;
