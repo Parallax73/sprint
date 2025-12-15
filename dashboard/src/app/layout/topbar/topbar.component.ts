18b50fc2b4bd9fbbd7183fa9f6c95a1a0d71db9d
Parallax73 <narakagaara@gmail.com>
2025-12-15 14:04:54 -0300

    feat(topbar): add "About Sprint" dialog with project details and GitHub link

--- dashboard/src/app/layout/topbar/topbar.component.ts
+++ dashboard/src/app/layout/topbar/topbar.component.ts
@@ -2,14 +2,21 @@ import { Component, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { LayoutService } from '../../core/services/layout.service';
 import {InputGroup} from 'primeng/inputgroup';
+import {Dialog} from 'primeng/dialog';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [ButtonModule, InputGroup],
+  imports: [ButtonModule, InputGroup, Dialog],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss'
 })
 export class TopbarComponent {
   layoutService = inject(LayoutService);
+
+  isHelpVisible = false;
+
+  showDialog() {
+    this.isHelpVisible = true;
+  }
 }
