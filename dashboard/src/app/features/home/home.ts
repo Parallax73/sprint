7346ecd64093acb353772b4c52d28289c6fd222a
Parallax73 <narakagaara@gmail.com>
2025-12-15 15:52:48 -0300

    feat(home): implement dynamic hub cards with structure, styles, and navigation links

--- dashboard/src/app/features/home/home.ts
+++ dashboard/src/app/features/home/home.ts
@@ -1,14 +1,53 @@
 import { Component } from '@angular/core';
-import {Sidebar} from '../../layout/sidebar/sidebar';
+import {RouterLink} from '@angular/router';
+
+interface HubCard {
+  title: string;
+  description: string;
+  link?: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [
-    Sidebar
-  ],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  imports: [
+    RouterLink
+  ],
+  styleUrls: ['./home.scss']
 })
 export class Home {
+
+  cards: HubCard[] = [
+    {
+      title: 'Dashboard',
+      description: 'View system health and performance metrics.',
+      link: '/dashboard'
+    },
+    {
+      title: 'Alerts',
+      description: 'View active security alerts, triage incidents, and investigate potential threats.',
+      link: '/alerts'
+    },
+    {
+      title: 'Detection Rules',
+      description: 'Manage detection logic, create new rules, and tune existing parameters.',
+      link: '/detection-rules'
+    },
+    {
+      title: 'Audit Logs',
+      description: 'Review system activity, track access logs, and monitor compliance events.',
+      link: '/audit-logs'
+    },
+    {
+      title: 'Settings',
+      description: 'Configure system preferences.',
+      link: '/settings'
+    },
+    {
+      title: 'Users',
+      description: 'Add, Remove, Update users, Manage roles and View user accounts.',
+      link: '/users'
+    }
+  ];
 
 }
