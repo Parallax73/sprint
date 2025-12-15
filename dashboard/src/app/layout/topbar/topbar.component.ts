8228127622cb6a1dc9050f60f779f59171c7444d
Parallax73 <narakagaara@gmail.com>
2025-12-15 12:43:03 -0300

    feat(layout): enhance topbar with search input and updated styles

--- dashboard/src/app/layout/topbar/topbar.component.ts
+++ dashboard/src/app/layout/topbar/topbar.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { LayoutService } from '../../core/services/layout.service';
+import {InputGroup} from 'primeng/inputgroup';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, InputGroup],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss'
 })
