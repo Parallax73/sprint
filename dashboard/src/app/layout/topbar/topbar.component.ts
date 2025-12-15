d3ce80ce01538d1ce0a288d1c30637169b2dd63e
Parallax73 <narakagaara@gmail.com>
2025-12-15 15:52:53 -0300

    refactor(topbar): remove unused InputGroup import to clean up dependencies

--- dashboard/src/app/layout/topbar/topbar.component.ts
+++ dashboard/src/app/layout/topbar/topbar.component.ts
@@ -1,13 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { LayoutService } from '../../core/services/layout.service';
-import {InputGroup} from 'primeng/inputgroup';
 import {Dialog} from 'primeng/dialog';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [ButtonModule, InputGroup, Dialog],
+  imports: [ButtonModule, Dialog],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss'
 })
